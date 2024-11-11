2b6ce4ecc1e91eaa6a298998b5c4379eaa6da69b
AP-Singhz <samrinder2002@gmail.com>
2024-11-11 11:04:29 -0500

    add firebase and also resolve the change for import paths

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
-import '@styles/global.css';
+import './styles/global.css';
+
 //everything is a functional component, which has a return statement that lets u use HTML
 //rmb to export the component so it can be used in otehr files
 export const metadata = {
