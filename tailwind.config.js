3bbc19aa45a91055ec6fa3c69e8c4df983cd8291
AP-Singhz <samrinder2002@gmail.com>
2024-11-18 19:36:39 -0500

    khjv

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 /* @type {import('tailwindcss').Config} */
-=======
-// @type {import('tailwindcss').Config} */
->>>>>>> 4ae6f947f71f873242117dc3bd64a0dcb8d49b35
 //slightly different than the premade one- got online.
 //this is just some css fonts
 
@@ -10,11 +6,7 @@ module.exports = {
   content: [
     './pages//.{js,ts,jsx,tsx,mdx}',
     './components/**/.{js,ts,jsx,tsx,mdx}',
-<<<<<<< HEAD
-    './src/app/**/*/.{js,ts,jsx,tsx,mdx}',
-=======
-    './src/app/*/.{js,ts,jsx,tsx,mdx}',
->>>>>>> 4ae6f947f71f873242117dc3bd64a0dcb8d49b35
+    './src/app/*//.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
