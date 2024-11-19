1da3431e042338688aba044992b53c5766073a86
AP-Singhz <samrinder2002@gmail.com>
2024-11-18 19:47:21 -0500

    delete profile folder

--- src/app/user/profile/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>PROFILE DETAILS: use dynamic nesting routing to idk show the page of each user profile u click on?</div>
-  )
-}
-
-export default page
